c9f9e389519471e13d28624fd8494309baac3170
justatestkd <96291328+justatestkd@users.noreply.github.com>
2021-12-17 16:43:51 +0300

    Update main.py

--- pkg/main.py
+++ pkg/main.py
@@ -2,3 +2,7 @@ import requests
 
 def getcodefromraw(link):
 	return requests.get(link).text
+
+def getgitcode(gitpath,filepath):
+	link = "https://raw.githubusercontent.com/"+gitpath+"/main/"+filepath
+	return requests.get(link).text
