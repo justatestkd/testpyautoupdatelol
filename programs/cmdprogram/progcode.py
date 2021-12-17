bc23438cb1465a6afb501d569a70c82d982cb16f
justatestkd <96291328+justatestkd@users.noreply.github.com>
2021-12-17 17:09:59 +0300

    Update progcode.py

--- programs/cmdprogram/progcode.py
+++ programs/cmdprogram/progcode.py
@@ -1,1 +1,24 @@
+import math
 
+while True:
+	cmd = input("Enter command here: ")
+	if cmd:
+		if cmd == "/exec" or cmd == "/execute":
+			code = input("Enter code for execute here: ")
+			if code:
+				exec(code)
+		if cmd == "/calc" or cmd == "/calculate" or cmd == "calculator":
+			opp = input("Choose a opperate(+ or -): ")
+			numbone = input("Number one: ")
+			numbtwo = input("Number two: ")
+			if opp:
+				if opp == "-":
+					if int(numbone) >= 0:
+						if int(numbtwo) >= 0:
+							result = int(numbone)-int(numbtwo)
+							print("Result: "+str(result))
+				if opp == "+":
+					if int(numbone) >= 0:
+						if int(numbtwo) >= 0:
+							result = int(numbone)+int(numbtwo)
+							print("Result: "+str(result))
